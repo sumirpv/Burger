9b333507ec591ab9f57cd7440c9f521a5daba28c
sumirpv <sumirpv@gmail.com>
2018-03-26 05:24:22 -0700

    added heroku db

--- config/connection.js
+++ config/connection.js
@@ -1,36 +1,22 @@
-// // Set up MySQL connection.
-// var mysql = require("mysql");
 
-// var connection = mysql.createConnection({
-//   port: 3306,
-//   host: "localhost",
-//   user: "root",
-//   password: "",
-//   database: "burger_db"
-// });
-
-// // Make connection.
-// connection.connect(function(err) {
-//   if (err) {
-//     console.error("error connecting: " + err.stack);
-//     return;
-//   }
-//   console.log("connected as id " + connection.threadId);
-// });
-
-// // Export connection for our ORM to use.
-// module.exports = connection;
 
 // Set up MySQL connection.
 var mysql = require("mysql");
+var connection;
 
-var connection = mysql.createConnection({
-  port: 3306,
+if(process.env.JAWSDB_URL){
+  connection = mysql.createConnection(process.env.JAWSDB_URL);
+}else{
+  connection = mysql.createConnection({
+  // port: 3306,
   host: "localhost",
   user: "root",
   password: "",
-  database: "burgers_db"
+  database: "burgers_db",
+  socketPath: '/Applications/MAMP/tmp/mysql/mysql.sock'
+
 });
+};
 
 // Make connection.
 connection.connect(function(err) {
@@ -42,4 +28,5 @@ connection.connect(function(err) {
 });
 
 // Export connection for our ORM to use.
+connection.connect();
 module.exports = connection;
