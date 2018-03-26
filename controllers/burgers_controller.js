bd429b9c3e7d00c6231467e3837f73d22135fa47
sumirpv <sumirpv@gmail.com>
2018-03-25 20:32:51 -0700

    added working files

---
+++ controllers/burgers_controller.js
@@ -0,0 +1,82 @@
+// // Import the ORM to create functions that will interact with the database.
+// var orm = require("../config/orm.js");
+
+// var burger = {
+//   all: function(cb) {
+//     orm.all("cats", function(res) {
+//       cb(res);
+//     });
+//   },
+//   // The variables cols and vals are arrays.
+//   create: function(cols, vals, cb) {
+//     orm.create("cats", cols, vals, function(res) {
+//       cb(res);
+//     });
+//   },
+//   update: function(objColVals, condition, cb) {
+//     orm.update("cats", objColVals, condition, function(res) {
+//       cb(res);
+//     });
+//   },
+//   delete: function(condition,vals, cb){
+//     orm.delete("cats",condition,vals , function(res){
+//       cb(res);
+//     })
+//   }
+// };
+
+// // Export the database functions for the controller (catsController.js).
+// module.exports = burger;
+
+
+
+var express = require("express");
+
+var router = express.Router();
+
+// Import the model (cat.js) to use its database functions.
+var burger = require("../models/burger.js");
+
+// Create all our routes and set up logic within those routes where required.
+router.get("/", function(req, res) {
+  burger.all(function(data) {
+    var hbsObject = {
+      burgers: data
+    };
+    console.log(hbsObject);
+    res.render("index", hbsObject);
+  });
+});
+
+router.post("/api/burgers", function(req, res) {
+  burger.create([
+    "burger_name", "devoured"
+  ], [
+    req.body.burger_name, req.body.devoured
+  ], function(result) {
+    // Send back the ID of the new quote
+    res.json({ id: result.insertId });
+  });
+});
+
+router.put("/api/burgers/:id", function(req, res) {
+  var condition = "id = " + req.params.id;
+
+  console.log("condition : ", condition);
+  console.log("devored "+req.body.devour)
+  burger.update({
+    devoured: req.body.devoured
+  }, condition, function(result) {
+    if (result.changedRows == 0) {
+      // If no rows were changed, then the ID must not exist, so 404
+      return res.status(404).end();
+    } else {
+      res.status(200).end();
+    }
+  });
+});
+
+
+
+// Export routes for server.js to use.
+module.exports = router;
---
+++ db/seeds.sql
@@ -0,0 +1,10 @@
+USE burgers_db;
+INSERT INTO burgers (burger_name ,devoured) VALUES ('Bacon Burger', true);
+INSERT INTO burgers (burger_name ,devoured) VALUES ('Veggie burger', true);
+INSERT INTO burgers (burger_name , devoured) VALUES ('chicken burger', true);
+INSERT INTO burgers (burger_name , devoured) VALUES ('tofu bueger', true);
+
+
+select * from burgers;
+
+
