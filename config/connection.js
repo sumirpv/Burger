4b51311a96e4fe2ea89f3a149ccc49fa96c955a0
sumirpv <sumirpv@gmail.com>
2018-03-26 07:13:12 -0700

    make it better

--- config/connection.js
+++ config/connection.js
@@ -8,7 +8,7 @@ if(process.env.JAWSDB_URL){
   connection = mysql.createConnection(process.env.JAWSDB_URL);
 }else{
   connection = mysql.createConnection({
-  // port: 3306,
+   port: 3306,
   host: "localhost",
   user: "root",
   password: "",
@@ -28,5 +28,5 @@ connection.connect(function(err) {
 });
 
 // Export connection for our ORM to use.
-connection.connect();
+//connection.connect();
 module.exports = connection;
